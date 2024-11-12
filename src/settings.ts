67b7df2a1bfa59e6b3eb46277ccebbd6eaa30d22
GershonGolubitsky <genomishe@gmail.com>
2024-11-12 18:05:24 +0200

    Add to Render

--- src/settings.ts
+++ src/settings.ts
@@ -7,21 +7,18 @@ export const Settings = [
     num_of_elevators: 3,
     num_of_floors: 25,
     timeInFloor: 2000,
-    timeToOpenDoor: 2000,
     name: 'building1',
   },
   {
     num_of_elevators: 1,
     num_of_floors: 5,
     timeInFloor: 2000,
-    timeToOpenDoor: 2000,
     name: 'building2',
   },
   {
     num_of_elevators: 2,
     num_of_floors: 10,
     timeInFloor: 2000,
-    timeToOpenDoor: 2000,
     name: 'building3',
   },
 ];
